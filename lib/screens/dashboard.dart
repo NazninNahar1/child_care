b8f16e687b7deea2e59ee9cc6e2f53e61b8a4aa9
NazninNahar <naznin.nahar.cse@gmail.com>
2023-01-26 18:23:13 +0600

    login update

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
@@ -21,31 +22,256 @@ class _DashboardState extends State<Dashboard> {
                   Container(
                     height: 60,
                     width: 315,
-
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
                       color: Colors.deepPurple,
                     ),
-                    child: const Center(child: Text('Home',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),),
+                    child: const Center(
+                      child: Text(
+                        'Home',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            Row(
-              children: [
-                Container
-                  (child: Image.asset('asset/doctorIcon.png')),
-                Container(
-                  height: 60,
-                  width: 315,
-
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.deepPurple,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0AFF2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'asset/doctorIcon.png',
+                      ),
+                    ),
                   ),
-                  child: const Center(child: Text('Home',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),),
-                ),
-              ],
+                  Container(
+                    height: 50,
+                    width: 280,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: Color(0xffF4DDFD),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Doctor List',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0AFF2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'asset/vaccineicon.png',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 280,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: Color(0xffF4DDFD),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Vaccine List',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0AFF2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'asset/diet.png',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 280,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: Color(0xffF4DDFD),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Child Diet',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0AFF2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'asset/doctorIcon.png',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 280,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: Color(0xffF4DDFD),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Doctor List',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0AFF2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'asset/doctorIcon.png',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 280,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: Color(0xffF4DDFD),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Doctor List',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0AFF2),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'asset/doctorIcon.png',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 280,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: Color(0xffF4DDFD),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Doctor List',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -53,4 +279,3 @@ class _DashboardState extends State<Dashboard> {
     );
   }
 }
-
